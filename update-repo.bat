0c1cd2cedf8026b48e1b791e9faa1ee78d48171d
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-03-10 22:15:39 +0700

    Update custom nodes repo to the lastest version

--- update-repo.bat
+++ update-repo.bat
@@ -5,8 +5,13 @@ git checkout main
 git add .
 git commit -a -m %1
 git push
+cd ..\comfy_ckpt_utils
+git checkout main
+git add .
+git commit -a -m %2
+git push
 cd ..
 git submodule update --remote --merge
 git add .
-if [%2] == [] (git commit -m "Update custom nodes repo to the lastest version") else (git commit -m %2)
+if [%2] == [] (git commit -m "Update custom nodes repo to the lastest version") else (git commit -m %3)
 git push
